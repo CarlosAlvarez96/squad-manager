1764b807ad13174ec9fd46e0e76259e74c05f23a
CarlosAlvarez96 <calvarezmartin96@gmail.com>
2024-03-22 11:07:45 +0100

    generate toke

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,15 +11,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
+#[Route('/user', name: 'app_user')]
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'user_hello', methods: ['GET'])]
-    public function hello(): Response
+    #[Route('/all', name: 'user_all', methods: ['GET'])]
+    public function getAllUsers(EntityManagerInterface $entityManager): JsonResponse
     {
-        return new Response('Hola mundo');
+        // Obtiene el repositorio de la entidad User
+        $userRepository = $entityManager->getRepository(User::class);
+
+        // Obtiene todos los usuarios
+        $users = $userRepository->findAll();
+
+        // Formatea los datos de los usuarios para la respuesta
+        $formattedUsers = [];
+        foreach ($users as $user) {
+            $formattedUsers[] = [
+                'id' => $user->getId(),
+                'email' => $user->getEmail(),
+                'password' => $user->getPassword()
+            ];
+        }
+
+        // Devuelve una respuesta JSON con todos los usuarios
+        return new JsonResponse($formattedUsers);
     }
     
-    #[Route('/user/register', name: 'user_register', methods: ['POST'])]
+    #[Route('/register', name: 'user_register', methods: ['POST'])]
     public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         // Obtiene los datos del cuerpo de la solicitud
